79e4baeec11a53dfee7b9f184d9493a68da05304
pythonunique <v.yazdani79@gmail.com>
2026-09-21 23:18:45 +0330

    fix: changing text location

--- src/components/AnimeDetail.jsx
+++ src/components/AnimeDetail.jsx
@@ -24,21 +24,22 @@ export default function AnimeDetail() {
       <p>vahdi yazdani</p>
       :
       <>
-      <div className='relative h-[calc(100vh-5rem)] w-full'>
-        <div className='absolute inset-0 block w-full'> 
-          <img  src={data.attributes.posterImage.original} className='w-full h-full object-contain  850:hidden right-0   ' />
+      <div className='relative h-[calc(100vh-5rem)] w-full '>
+        <div className='absolute inset-0 block w-full opacity-40'> 
+          <img  src={data.attributes.posterImage.original} className='w-full h-full object-contain  850:hidden right-0 ' />
           <img  src={data.attributes.coverImage.original} className='hidden 850:block w-full h-full object-cover animate-[fadeIn_0.8s_ease-in]' />
 
         </div>
         <div className='absolute inset-0 bg-linear-to-r from-black via-black/1 to-transparent'></div> 
-        <div className='absolute inset-0 bg-linear-to-l from-black via-black/1 to-transparent'></div> 
-      </div>
+        <div className='absolute inset-0 bg-linear-to-l from-black via-black/1 to-transparent'></div>
 
-      <div className='flex flex-col mx-auto max-w-6xl px-5 py-10'>
+
+
+      <div className='absolute flex flex-col mx-auto w-full top-1 sm:top-1/2'>
         <p className='text-primary text-center text-2xl font-header mt-3 sm:text-3xl md:text-5xl'>{data.attributes.canonicalTitle}</p>
       <p className='text-center px-10 font-pop sm:font-medium md:font-bold mx-auto mt-5 max-w-4xl leading-7 text-white/70'>{data.attributes.synopsis}</p>
       </div>
-      <div className='flex flex-wrap items-center gap-4 justify-center'>
+      <div className='bottom-5  absolute flex flex-wrap gap-4 w-full justify-center '>
         <span className='badge badge-primary font-pop'>{data.attributes.averageRating}</span>
         <span className='badge badge-primary font-pop'>{data.attributes.subtype}</span>
         <span className='badge badge-primary font-pop'>{data.attributes.episodeCount}</span>
@@ -46,6 +47,17 @@ export default function AnimeDetail() {
         <span className='badge badge-primary font-pop'>{data.attributes.startDate}</span>
         <span className='badge badge-primary font-pop'>{data.attributes.endDate}</span>
       </div>
+
+
+
+
+
+      </div>
+
+
+
+
+
       </>
       }
 
